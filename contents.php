1b46f9c4a3b1e1c3d279c28dcae0010e20a41d0d
Matt Pass <matt@mattpass.com>
2012-08-08 14:04:29 +0100

    Shows SHA's, changed, new & deleted files/folders
    3 columns now, for server dir list, repo list and compare list
    Build up 3 arrays for dir list (file/folder name), SHA of file/folder
    and type (file/dir)
    Echo these out to similarly named JS arrays
    Build up 2 JS arrays for the repo list (file/folder name) and SHA of
    file/folder
    Fill our server dir list & repo list columns with file/folder name and
    the SHA of it's content
    With that done, build up a compare list of changed files, new
    files/folders & deleted files/folders
    Finally write that to the compare list DIV

--- contents.php
+++ contents.php
@@ -22,11 +22,9 @@ function numClean($var) {
 <script src="lib/github.js"></script>
 </head>
 
-<body style="margin: 20px">
-
-<div id="repoList" style="position: relative: left: 0px; float: left; padding-right: 100px"></div>
+<body style="margin: 20px; font-family: arial, helvetica, swiss, verdana; font-size: 12px">
 	
-<div id="dirList" styele="position: relative; left: 1000px; float: left">SERVER DIR LIST:<br><br>
+<div id="dirList" style="position: relative; float: left; padding-right: 100px"><b style="font-size: 18px">SERVER DIR LIST:</b><br><br>
 <?php
 // Function to sort given values alphabetically
 function alphasort($a, $b) {
@@ -53,15 +51,49 @@ $objectList = new SortingIterator(new RecursiveIteratorIterator(new RecursiveDir
 
 // Finally, we have our ordered list, so display
 $i=0;
+$dirListArray = array();
+$dirSHAArray = array();
+$dirTypeArray = array();
 foreach ($objectList as $objectRef) {
 	$fileFolderName = rtrim(substr($objectRef->getPathname(), strlen($path)),"..");
 	if ($objectRef->getFilename()!="." && $fileFolderName[strlen($fileFolderName)-1]!="/") {
-			echo $i." : ".ltrim($fileFolderName,"/")."<br>";
+			$contents = file_get_contents($path.$fileFolderName);
+			$store = "blob ".strlen($contents)."\0".$contents;
+			echo $i." : ".ltrim($fileFolderName,"/")."<br>".sha1($store)."<br><br>";
 			$i++;
+			array_push($dirListArray,ltrim($fileFolderName,"/"));
+			array_push($dirSHAArray,sha1($store));
+			$type = is_dir($path.$fileFolderName) ? "dir" : "file";
+			array_push($dirTypeArray,$type);
 	}
 }
+
+echo PHP_EOL.PHP_EOL.'<script>'.PHP_EOL;
+echo 'dirListArray = [';
+for ($i=0;$i<count($dirListArray);$i++) {
+	echo "'".$dirListArray[$i]."'";
+	if ($i<count($dirListArray)-1) {echo ",";};
+}
+echo '];'.PHP_EOL;
+echo 'dirSHAArray = [';
+for ($i=0;$i<count($dirSHAArray);$i++) {
+	echo "'".$dirSHAArray[$i]."'";
+	if ($i<count($dirSHAArray)-1) {echo ",";};
+}
+echo '];'.PHP_EOL;
+echo 'dirTypeArray = [';
+for ($i=0;$i<count($dirTypeArray);$i++) {
+	echo "'".$dirTypeArray[$i]."'";
+	if ($i<count($dirTypeArray)-1) {echo ",";};
+}
+echo '];'.PHP_EOL;
+echo '</script>';
 ?>
 </div>
+
+<div id="repoList" style="position: relative: left: 0px; float: left; padding-right: 100px"></div>
+	
+<div id="compareList" style="position: relative; float: left"></div>
 	
 <script>
 var github = new Github(<?php
@@ -71,6 +103,8 @@ if ($token!="") {
 	echo '{username: "'.strClean($_POST['$username']).'", password: "'.strClean($_POST['$password']).'", auth: "basic"}';
 }?>);
 
+repoListArray = [];
+repoSHAArray = [];
 gitCommand = function(comm,value) {
 	if (comm=="repo.show") {
 		repoDir = value.split("@");
@@ -79,14 +113,43 @@ gitCommand = function(comm,value) {
 		dir = repoDir[1];		
 		var repo = github.getRepo(user,repo);
 		var user = github.getUser();
-		var repoList = "REPO LIST (Github):<br><br>";
+		var repoList = "<b style='font-size: 18px'>REPO LIST (Github):</b><br><br>";
+		var compareList = "COMPARE LIST:<br><br>";
 		document.getElementById('repoList').innerHTML = "";
+		document.getElementById('compareList').innerHTML = "";
  		repo.getTree('master?recursive=true', function(err, tree) {
 			for (i=0;i<tree.length;i++) {
-				repoList += i + " : " + tree[i].path + "<br>";
+				repoList += i + " : " + tree[i].path + "<br>" + tree[i].sha + "<br><br>";
+				repoListArray.push(tree[i].path);
+				repoSHAArray.push(tree[i].sha);
 			}
 			document.getElementById('repoList').innerHTML = repoList;
-			console.log(tree)}
+			console.log(tree);
+			compareList += "<b style='font-size: 18px'>CHANGED FILES:</b><br><br>"
+			newFilesList = "";
+			for (i=0;i<dirListArray.length;i++) {
+				repoArrayPos = repoListArray.indexOf(dirListArray[i]);
+				if (repoArrayPos == "-1") {
+					newFilesList += i+" : "+dirListArray[i]+"<br>";
+				} else if (dirTypeArray[i] == "file" && dirSHAArray[i] != repoSHAArray[repoArrayPos]) {
+					compareList += i+" = "+repoArrayPos+" : "+dirListArray[i]+"<br>";
+				}
+			}
+			
+			compareList += "<br><b style='font-size: 18px'>NEW FILES:</b><br><br>"+newFilesList;
+			
+			delFilesList = "";
+			for (i=0;i<repoListArray.length;i++) {
+				dirArrayPos = dirListArray.indexOf(repoListArray[i]);
+				if (dirArrayPos == "-1") {
+					delFilesList += i+" : "+repoListArray[i]+"<br>";
+				}
+			}
+			
+			compareList += "<br><b style='font-size: 18px'>DELETED FILES:</b><br><br>"+delFilesList;
+			
+			document.getElementById('compareList').innerHTML = compareList;
+			}
 		)
 	}
 }
