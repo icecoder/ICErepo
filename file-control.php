db2cb747704e9defb43791595e7a3e38917d37dd
Matt Pass <matt@mattpass.com>
2012-08-17 08:02:43 +0100

    Updating changed files & multiple processing
    $path & $action vars now also received
    Fixed broken user auth (token, username & password syntax/bugs)
    User auth now done in isolation before anything else (ie, out of
    action=view condition)
    Setting $fileContents now moved to action=view condition
    fcForm moved to both the action=view and else block
    else block also establishes arrays from comma seperated lists for row,
    repo, dir & action
    textareas created and filled with file contents everytime there is a dir
    in the array
    Comma seperated lists drawn up and redumped into JS arrays for row,
    repo, dir & action
    JS then takes over now it has all files loaded into textareas and array
    lists to run through
    Functions added ffAddOrUpdate and ffDelete
    The first here will receive params and then repo.write to Github
    If all went well it will hide the row and (which also removes the first
    items from our 4 arrays, unchecks the checkbox and redo's parent level
    arrays)
    If we have more items left to process (rowID), start the process again
    startProcess funcion also handles adding files by setting the right
    param values
    Has placeholder to also handle deleting files.

--- file-control.php
+++ file-control.php
@@ -14,9 +14,11 @@ function numClean($var) {
 	return is_numeric($var) ? floatval($var) : false;
 }
 
-$rowID = numClean($_POST['rowID']);
+$path = strClean($_POST['path']);
+$rowID = strClean($_POST['rowID']);
 $repo = strClean($_POST['repo']);
 $dir = strClean($_POST['dir']);
+$action = strClean($_POST['action']);
 
 echo "<script>fullRepoPath='".$repo."';</script>";
 ?>
@@ -33,91 +35,147 @@ echo "<script>fullRepoPath='".$repo."';</script>";
 
 <body>
 	
-<?php
-$fileContents = file_get_contents($dir);
-?>
-
-<form name="fcForm">
-<textarea name="fileContents"><?php echo htmlentities($fileContents); ?></textarea>
-<textarea name="repoContents"></textarea>
-</form>
-
 <script>
-<?php if ($_POST['action']=="view") {?>
-var github = new Github(<?php
-if ($token!="") {
-	echo '{token: "'.strClean($_POST['token']).'", auth: "oauth"}';
-} else{
-	echo '{username: "'.strClean($_POST['$username']).'", password: "'.strClean($_POST['$password']).'", auth: "basic"}';
-}?>);
-rowID = <?php echo $rowID; ?>;
-repoUser = fullRepoPath.split('/')[0];
-repoName = fullRepoPath.split('/')[1];
-filePath = fullRepoPath.replace(repoUser+"/"+repoName+"/","");
-var repo = github.getRepo(repoUser,repoName);
-sendData = function() {
-	repo.read('master', filePath, function(err, data) {
-		document.fcForm.repoContents.innerHTML=data;
-		dirContent = document.fcForm.fileContents.value;
-		repoContent = document.fcForm.repoContents.value;
-		diffUsingJS(dirContent,repoContent);
-		parent.document.getElementById("row"+rowID+"Content").style.display = "inline-block";
-	});
-}
-	
-function diffUsingJS (dirContent,repoContent) {
-	var base = difflib.stringAsLines(dirContent);
-	var newtxt = difflib.stringAsLines(repoContent);
-	var sm = new difflib.SequenceMatcher(base, newtxt);
-	var opcodes = sm.get_opcodes();
-	var diffoutputdiv = parent.document.getElementById("row"+rowID+"Content");
-	while (diffoutputdiv.firstChild) diffoutputdiv.removeChild(diffoutputdiv.firstChild);
-	var contextSize = ""; // optional
-	contextSize = contextSize ? contextSize : null;
-	diffoutputdiv.appendChild(
-		diffview.buildView(
-			{
-			baseTextLines:base,
-			newTextLines:newtxt,
-			opcodes:opcodes,
-			baseTextName:"Server:     <?php echo str_replace($_SERVER['DOCUMENT_ROOT']."/","",$dir);?>     ",
-			newTextName:"Github:     <?php echo $repo;?>",
-			contextSize:contextSize,
-			viewType: 1 // 0 = side by side, 1 = inline
-			}
-		)
-	)
-}
-	
-sendData();
-<?php ;}; ?>
-<?php if ($_POST['action']=="changed") {?>
-	// Update files...
-	console.log('UPDATE PROCESS...');
-	parent.document.getElementById('row'+<?php echo $rowID;?>).style.display = "none";
-	parent.document.getElementById('row'+<?php echo $rowID;?>+'Content').style.display = "none";
-<?php ;}; ?>
-<?php if ($_POST['action']=="new") {?>
-	// Add files...
-	console.log('ADD PROCESS...');
-	parent.document.getElementById('row'+<?php echo $rowID;?>).style.display = "none";
-	parent.document.getElementById('row'+<?php echo $rowID;?>+'Content').style.display = "none";
-<?php ;}; ?>
-<?php if ($_POST['action']=="deleted") {?>
-	// Delete files...
-	console.log('DELETE PROCESS...');
-	parent.document.getElementById('row'+<?php echo $rowID;?>).style.display = "none";
-	parent.document.getElementById('row'+<?php echo $rowID;?>+'Content').style.display = "none";
-<?php ;}; ?>
-<?php
-	echo "console.log('".$_POST['action']."');";
-	echo "console.log('".$_POST['title']."');";
-	echo "console.log('".$_POST['message']."');";
-	echo "console.log('".$rowID."');";
-	echo "console.log('".$repo."');";
-	echo "console.log('".$dir."');";
-?>
+	var github = new Github(<?php
+	if ($_POST['token']!="") {
+		echo '{token: "'.strClean($_POST['token']).'", auth: "oauth"}';
+	} else{
+		echo '{username: "'.strClean($_POST['username']).'", password: "'.strClean($_POST['password']).'", auth: "basic"}';
+	}?>);
+	repoUser = fullRepoPath.split('/')[0];
+	repoName = fullRepoPath.split('/')[1];
+	filePath = fullRepoPath.replace(repoUser+"/"+repoName+"/","");
+	var repo = github.getRepo(repoUser,repoName);
 </script>
+
+<?php if ($_POST['action']=="view") {
+	$fileContents = file_get_contents($dir); ?>
+
+	<form name="fcForm">
+	<textarea name="fileContents"><?php echo htmlentities($fileContents); ?></textarea>
+	<textarea name="repoContents"></textarea>
+	</form>
+	
+	<script>
+	rowID = <?php echo $rowID; ?>;
+	sendData = function() {
+		console.log(filePath);
+		repo.read('master', filePath, function(err, data) {
+			document.fcForm.repoContents.innerHTML=data;
+			dirContent = document.fcForm.fileContents.value;
+			repoContent = document.fcForm.repoContents.value;
+			diffUsingJS(dirContent,repoContent);
+			parent.document.getElementById("row"+rowID+"Content").style.display = "inline-block";
+		});
+	}
+		
+	function diffUsingJS (dirContent,repoContent) {
+		var base = difflib.stringAsLines(dirContent);
+		var newtxt = difflib.stringAsLines(repoContent);
+		var sm = new difflib.SequenceMatcher(base, newtxt);
+		var opcodes = sm.get_opcodes();
+		var diffoutputdiv = parent.document.getElementById("row"+rowID+"Content");
+		while (diffoutputdiv.firstChild) diffoutputdiv.removeChild(diffoutputdiv.firstChild);
+		var contextSize = ""; // optional
+		contextSize = contextSize ? contextSize : null;
+		diffoutputdiv.appendChild(
+			diffview.buildView(
+				{
+				baseTextLines:base,
+				newTextLines:newtxt,
+				opcodes:opcodes,
+				baseTextName:"Server:     <?php echo str_replace($_SERVER['DOCUMENT_ROOT']."/","",$dir);?>     ",
+				newTextName:"Github:     <?php echo $repo;?>",
+				contextSize:contextSize,
+				viewType: 1 // 0 = side by side, 1 = inline
+				}
+			)
+		)
+	}
+		
+	sendData();
+	</script>
+<?php } else { ?>
+	<?php
+	$rowIDArray = explode(",",$rowID);
+	$repoArray = explode(",",$repo);
+	$dirArray = explode(",",$dir);
+	$actionArray = explode(",",$action);
+	?>
+	<form name="fcForm">
+	<?php
+	for ($i=0;$i<count($rowIDArray);$i++) {
+		if ($dirArray[$i]!="") {
+			$fileContents = file_get_contents($dirArray[$i]);
+			echo '<textarea name="fileContents'.$rowIDArray[$i].'">'.htmlentities($fileContents).'</textarea>';
+		}
+	}
+	?>
+	</form>
+	<script>
+	<?php
+	$rowIDVal = $repoVal = $dirVal = $actionVal = "";
+	for ($i=0;$i<count($rowIDArray);$i++) {
+		$rowIDVal .= $rowIDArray[$i];
+		$repoVal .= "'".$repoArray[$i]."'";
+		$dirVal .= "'".$dirArray[$i]."'";
+		$actionVal .= "'".$actionArray[$i]."'";
+		if ($i<count($rowIDArray)-1) {
+			$rowIDVal .= ",";
+			$repoVal .= ",";
+			$dirVal .= ",";
+			$actionVal .= ",";
+		}
+	}
+	?>
+	rowIDArray = [<?php echo $rowIDVal;?>];
+	repoArray = [<?php echo $repoVal;?>];
+	dirArray = [<?php echo $dirVal;?>];
+	actionArray = [<?php echo $actionVal;?>];
+
+	// Add or Update files...
+	ffAddOrUpdate = function(row,gitRepo,action) {
+		console.log('UPDATE PROCESS...'+row+','+gitRepo+','+action);
+		repo.write('master', gitRepo, document.fcForm['fileContents'+row].value, '<?php echo strClean($_POST['title']); ?>\n\n<?php echo strClean($_POST['message']); ?>', function(err) {
+			if(!err) {
+				hideRow(row);
+				if (rowIDArray.length>0) {
+					startProcess();
+				}
+			} else {
+				alert('Sorry, there was an error adding '+gitRepo);
+			}
+		});
+	}
+	// Delete files...
+	ffDelete = function(row,repo,action) {
+		console.log('DELETE PROCESS...'+row+','+repo+','+action);
+		hideRow(row);
+	}
+		
+	hideRow = function(row) {
+		rowIDArray.splice(0,1);
+		repoArray.splice(0,1);
+		dirArray.splice(0,1);
+		actionArray.splice(0,1);
+		parent.document.getElementById('checkbox'+row).checked=false;
+		parent.updateSelection(parent.document.getElementById('checkbox'+row));
+		parent.document.getElementById('row'+row).style.display = parent.document.getElementById('row'+row+'Content').style.display = "none";
+	}
+
+	startProcess = function() {
+		if(actionArray[0]=="changed"||actionArray[0]=="new") {
+			if(actionArray[0]=="changed")	{repoLoc = repoArray[0].replace(repoUser+"/"+repoName+"/","")}
+			if(actionArray[0]=="new")		{repoLoc = dirArray[0].replace('<?php echo $path;?>/','')}
+			ffAddOrUpdate(rowIDArray[0],repoLoc,actionArray[0]);
+		}
+		if(actionArray[0]=="deleted") {
+			ffDelete(rowIDArray[0],repoArray[0],actionArray[0]);
+		}
+	}
+	startProcess();
+	</script>
+<?php } ?>
 	
 </body>
 	
