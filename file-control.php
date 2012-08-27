23839676eb4434487b165cf1bda72376dab18e5c
Matt Pass <matt@mattpass.com>
2012-08-27 15:09:43 +0100

    Simplification & moving JS functions
    4 x arrays created at top rather than within if conditions
    JavaScript functions moved to ice-repo.js file
    Simplified getting file contents without var creation
    CSV strings created with implode now
    Simplifed a lot of script calls to contain whole blocks within a JS
    block, so no need to write <script> and </script> many times

--- file-control.php
+++ file-control.php
@@ -20,6 +20,10 @@ $rowID = strClean($_POST['rowID']);
 $repo = strClean($_POST['repo']);
 $dir = strClean($_POST['dir']);
 $action = str_replace("PULL:","",str_replace("SAVEPULLS:","",strClean($_POST['action'])));
+$rowIDArray = explode(",",$rowID);
+$repoArray = explode(",",$repo);
+$dirArray = explode(",",$dir);
+$actionArray = explode(",",$action);
 ?>
 <!DOCTYPE html>
 <html>
@@ -49,74 +53,20 @@ $action = str_replace("PULL:","",str_replace("SAVEPULLS:","",strClean($_POST['ac
 	repoName = gitRepo.split('/')[1];
 	filePath = fullRepoPath.replace(repoUser+"/"+repoName+"/","");
 	var repo = github.getRepo(repoUser,repoName);
-	
-	removeFirstArrayItems = function() {
-		rowIDArray.splice(0,1);
-		repoArray.splice(0,1);
-		dirArray.splice(0,1);
-		actionArray.splice(0,1);	
-	}
-		
-	hideRow = function(row) {
-		top.rowCount--;
-		updateInfo('parent');
-		get('checkbox'+row,'parent').checked=false;
-		parent.updateSelection(get('checkbox'+row,'parent'));
-		get('row'+row,'parent').style.display = get('row'+row+'Content','parent').style.display = "none";
-	}
 </script>
 
-<?php if ($_POST['action']=="view") {
-	$fileContents = file_get_contents($dir); ?>
-
+<?php if ($_POST['action']=="view") {?>
 	<form name="fcForm">
-	<textarea name="fileContents"><?php echo htmlentities($fileContents); ?></textarea>
+	<textarea name="fileContents"><?php echo htmlentities(file_get_contents($dir)); ?></textarea>
 	</form>
-	
 	<script>
 	rowID = <?php echo $rowID; ?>;
-	sendData = function() {
-		repo.read('master', filePath, function(err, data) {
-			dirContent = document.fcForm.fileContents.value;
-			repoContent = data;
-			diffUsingJS(dirContent,repoContent);
-			get("row"+rowID+"Content","parent").style.display = "inline-block";
-		});
-	}
 		
-	function diffUsingJS (dirContent,repoContent) {
-		var base = difflib.stringAsLines(dirContent);
-		var newtxt = difflib.stringAsLines(repoContent);
-		var sm = new difflib.SequenceMatcher(base, newtxt);
-		var opcodes = sm.get_opcodes();
-		var diffoutputdiv = get("row"+rowID+"Content","parent");
-		while (diffoutputdiv.firstChild) diffoutputdiv.removeChild(diffoutputdiv.firstChild);
-		var contextSize = ""; // optional
-		contextSize = contextSize ? contextSize : null;
-		diffoutputdiv.appendChild(
-			diffview.buildView(
-				{
-				baseTextLines:base,
-				newTextLines:newtxt,
-				opcodes:opcodes,
-				baseTextName:"Server:     <?php echo str_replace($_SERVER['DOCUMENT_ROOT']."/","",$dir);?>     ",
-				newTextName:"Github:     <?php echo $repo;?>",
-				contextSize:contextSize,
-				viewType: 1 // 0 = side by side, 1 = inline
-				}
-			)
-		)
-	}
-		
-	sendData();
+	baseTextName = "Server:     <?php echo str_replace($_SERVER['DOCUMENT_ROOT']."/","",$dir);?>     ";
+	newTextName = "Github:     <?php echo $repo;?>";
+	window.onLoad = sendData(baseTextName,newTextName);
 	</script>
 <?php } else if (substr($_POST['action'],0,5)=="PULL:") { ?>
-	<?php
-	$rowIDArray = explode(",",$rowID);
-	$repoArray = explode(",",$repo);
-	$dirArray = explode(",",$dir);
-	$actionArray = explode(",",$action);
-	?>
 	<form name="fcForm" action="file-control.php" method="POST">
 	<?php
 	echo '<input type="hidden" name="rowID" value="'.$rowID.'">';
@@ -132,49 +82,15 @@ $action = str_replace("PULL:","",str_replace("SAVEPULLS:","",strClean($_POST['ac
 	?>
 	</form>
 	<script>
-	<?php
-	$rowIDVal = $repoVal = $dirVal = $actionVal = "";
-	for ($i=0;$i<count($rowIDArray);$i++) {
-		$rowIDVal .= $rowIDArray[$i];
-		$repoVal .= "'".$repoArray[$i]."'";
-		$dirVal .= "'".$dirArray[$i]."'";
-		$actionVal .= "'".$actionArray[$i]."'";
-		if ($i<count($rowIDArray)-1) {
-			$rowIDVal .= ",";
-			$repoVal .= ",";
-			$dirVal .= ",";
-			$actionVal .= ",";
-		}
-	}
-	?>
-	rowIDArray = [<?php echo $rowIDVal;?>];
-	repoArray = [<?php echo $repoVal;?>];
-	dirArray = [<?php echo $dirVal;?>];
-	actionArray = [<?php echo $actionVal;?>];
-	getData = function() {
-		if (actionArray[0]!="new") {
-			repo.read('master', repoArray[0], function(err, data) {
-				document.fcForm['repoContents'+rowIDArray[0]].innerHTML=data;
-				if(!err) {
-					removeFirstArrayItems();
-					rowIDArray.length>0 ? getData() : document.fcForm.submit();
-				} else {
-					alert('Sorry, there was an error reading '+repoArray[0]);
-				}
-			});
-		} else {
-			removeFirstArrayItems();
-			rowIDArray.length>0 ? getData() : document.fcForm.submit();
-		}
-	}
-	getData();
+	rowIDArray = [<?php echo implode(",", $rowIDArray);?>];
+	repoArray = [<?php echo "'".implode("','", $repoArray)."'";?>];
+	dirArray = [<?php echo "'".implode("','", $dirArray)."'";?>];
+	actionArray = [<?php echo "'".implode("','", $actionArray)."'";?>];
+	window.onLoad = getData();
 	</script>
-<?php } else if (substr($_POST['action'],0,10)=="SAVEPULLS:") { ?>
+<?php } else if (substr($_POST['action'],0,10)=="SAVEPULLS:") {?>
+	<script>
 	<?php
-		$rowIDArray = explode(",",$rowID);
-		$repoArray = explode(",",$repo);
-		$dirArray = explode(",",$dir);
-		$actionArray = explode(",",$action);
 		for ($i=0;$i<count($rowIDArray);$i++) {
 			if ($actionArray[$i]!="new") {
 				$dirs = explode("/",$repoArray[$i]);
@@ -185,99 +101,41 @@ $action = str_replace("PULL:","",str_replace("SAVEPULLS:","",strClean($_POST['ac
 						mkdir($path.$relDir, 0755);
 					}
 				}
-				$fh = fopen($path."/".$repoArray[$i], 'w') or die("<script>alert('Sorry, there was a problem pulling ".$repoArray[$i].". Either the file is unavailable on Github or server permissions aren\'t allowing it to be created/updated.');get('blackMask','top').style.display='none';</script>");
+				$fh = fopen($path."/".$repoArray[$i], 'w') or die("alert('Sorry, there was a problem pulling ".$repoArray[$i].". Either the file is unavailable on Github or server permissions aren\'t allowing it to be created/updated.');get('blackMask','top').style.display='none';");
 				fwrite($fh, $_POST['repoContents'.$rowIDArray[$i]]);
 				fclose($fh);
-// 				echo "<script>removeFirstArrayItems()</script>";
-				echo "<script>hideRow(".$rowIDArray[$i].");top.newCount--;</script>";
+				echo "hideRow(".$rowIDArray[$i].");top.newCount--;";
 			} else {
 				is_dir($dir) ? $success = rmdir($dir) : $success = unlink($dir);
 				if (!$success) {
-					echo "<script>alert('Sorry, couldn\'t delete ".$dir."\\n\\nMaybe you need to give file permissions for it to be deleted?')</script>";
+					echo "alert('Sorry, couldn\'t delete ".$dir."\\n\\n";
+					echo "Maybe you need to give file permissions for it to be deleted?');";
 				} else {
-					echo "<script>hideRow(".$rowIDArray[$i].");top.deletedCount--;</script>";
+					echo "hideRow(".$rowIDArray[$i].");top.deletedCount--;";
 				}
 			}
 		}
-		echo "<script>get('blackMask','top').style.display = 'none'</script>";
+		echo "get('blackMask','top').style.display = 'none';";
 	?>
+	</script>
 <?php } else { ?>
-	<?php
-	$rowIDArray = explode(",",$rowID);
-	$repoArray = explode(",",$repo);
-	$dirArray = explode(",",$dir);
-	$actionArray = explode(",",$action);
-	?>
 	<form name="fcForm">
 	<?php
 	for ($i=0;$i<count($rowIDArray);$i++) {
 		if ($dirArray[$i]!="") {
-			$fileContents = file_get_contents($dirArray[$i]);
-			echo '<textarea name="fileContents'.$rowIDArray[$i].'">'.htmlentities($fileContents).'</textarea>';
+			echo '<textarea name="fileContents'.$rowIDArray[$i].'">';
+			echo htmlentities(file_get_contents($dirArray[$i]));
+			echo '</textarea>';
 		}
 	}
 	?>
 	</form>
 	<script>
-	<?php
-	$rowIDVal = $repoVal = $dirVal = $actionVal = "";
-	for ($i=0;$i<count($rowIDArray);$i++) {
-		$rowIDVal .= $rowIDArray[$i];
-		$repoVal .= "'".$repoArray[$i]."'";
-		$dirVal .= "'".$dirArray[$i]."'";
-		$actionVal .= "'".$actionArray[$i]."'";
-		if ($i<count($rowIDArray)-1) {
-			$rowIDVal .= ",";
-			$repoVal .= ",";
-			$dirVal .= ",";
-			$actionVal .= ",";
-		}
-	}
-	?>
-	rowIDArray = [<?php echo $rowIDVal;?>];
-	repoArray = [<?php echo $repoVal;?>];
-	dirArray = [<?php echo $dirVal;?>];
-	actionArray = [<?php echo $actionVal;?>];
-
-	// Add or Update files...
-	ffAddOrUpdate = function(row,gitRepo,action) {
-		repo.write('master', gitRepo, document.fcForm['fileContents'+row].value, '<?php echo strClean($_POST['title']); ?>\n\n'+parent.document.fcForm.message.value, function(err) {
-			if(!err) {
-				removeFirstArrayItems();
-				hideRow(row);
-				top.newCount--;
-				rowIDArray.length>0 ? startProcess() : get('blackMask','top').style.display = "none";
-			} else {
-				alert('Sorry, there was an error adding '+gitRepo);
-			}
-		});
-	}
-	// Delete files...
-	ffDelete = function(row,gitRepo,action) {
-		repo.remove('master', gitRepo, function(err) {
-			if(!err) {
-				removeFirstArrayItems();
-				hideRow(row);
-				top.deletedCount--;
-				rowIDArray.length>0 ? startProcess() : get('blackMask','top').style.display = "none";
-			} else {
-				alert('Sorry, there was an error deleting '+gitRepo);
-			}
-		});
-	}
-
-	startProcess = function() {
-		if(actionArray[0]=="changed"||actionArray[0]=="new") {
-			if(actionArray[0]=="changed")	{repoLoc = repoArray[0].replace(repoUser+"/"+repoName+"/","")}
-			if(actionArray[0]=="new")		{repoLoc = dirArray[0].replace('<?php echo $path;?>/','')}
-			ffAddOrUpdate(rowIDArray[0],repoLoc,actionArray[0]);
-		}
-		if(actionArray[0]=="deleted") {
-			repoLoc = repoArray[0].replace(repoUser+"/"+repoName+"/","");
-			ffDelete(rowIDArray[0],repoLoc,actionArray[0]);
-		}
-	}
-	startProcess();
+	rowIDArray = [<?php echo implode(",", $rowIDArray);?>];
+	repoArray = [<?php echo "'".implode("','", $repoArray)."'";?>];
+	dirArray = [<?php echo "'".implode("','", $dirArray)."'";?>];
+	actionArray = [<?php echo "'".implode("','", $actionArray)."'";?>];
+	window.onLoad = startProcess();
 	</script>
 <?php } ?>
 	
