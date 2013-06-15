6935ba04b7913be043e0eb19e4fe3d038fb913e5
Matt Pass <matt@mattpass.com>
2013-06-15 20:07:55 +0100

    Common proceses moved out & new loading screen
    Session handling, docRoot and version no moved to new settings file
    You now set token, username and password also in settings file along
    with repo list
    New loading screen added
    No need to post token, username and password around now

--- index.php
+++ index.php
@@ -1,33 +1,8 @@
-<?php
-session_start();
-// $_SESSION['userLevel'] = 10;
-if ($_SESSION['userLevel'] == 0) {
-	die("Sorry, you need to be logged in to use ICErepo");
-}
-
-$docRoot = $_SERVER['DOCUMENT_ROOT'];
-$version = "0.7.2";
-
-// AUTHENTICATION
-// Can either be done by oauth, or username & password.
-// oauth
-$token = "";
-// Basic
-$username = "username";
-$password = "password";
-
-// REPOS & SERVER DIRS
-// Here you identify the repo location and related path on your server
-// (the last param is to identify which dropdown option to select by default).
-$repos = array(
-		"mattpass/dirTree",$docRoot."/dirTree","",
-		"mattpass/CodeMirror2",$docRoot."/CodeMirror2","selected"
-		);
-?>
+<?php include("settings.php"); ?>
 <!DOCTYPE html>
 <html>
 <head>
-<title>ICErepo v<?php echo $version;?></title>
+<title>ICErepo v <?php echo $version;?></title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <script src="lib/base64.js"></script>
 <script src="lib/github.js"></script>
@@ -37,10 +12,12 @@ $repos = array(
 
 <body style="margin: 0; overflow: hidden" onLoad="doRepo(get('repos').value)">
 	
-<div class="blackMask" id="blackMask" style="display: block">
-	<div id="loadingMsgCenter" class="loadingMsgCenter">
-		<div id="loadingMsgContainer" class="loadingMsgContainer">
-		WORKING...
+<div id="loadingMask" class="blackMask" style="visibility: visible" onContextMenu="return false">
+	<div class="popupVCenter">
+		<div class="popup">
+			<div class="circleOutside"></div>
+			<div class="circleInside"></div>
+			&nbsp;&nbsp;&nbsp;working...
 		</div>
 	</div>
 </div>
@@ -62,10 +39,7 @@ $repos = array(
 </div>
 
 <form name="showRepo" action="contents.php" target="repo" method="POST">
-<input type="hidden" name="token" value="<?php echo $token;?>">
-<input type="hidden" name="username" value="<?php echo $username;?>">
-<input type="hidden" name="password" value="<?php echo $password;?>">
-<input type="hidden" name="repo" value="">
+	<input type="hidden" name="repo" value="">
 </form>
 
 <iframe id="repo" class="repoFrame" frameborder="0"></iframe>
