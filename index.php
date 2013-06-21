eb60b5133b01ae3c82f51855b48fdd95f34313cc
Matt Pass <matt@mattpass.com>
2013-06-21 19:58:25 +0100

    Session login screen added
    If we have the sessionLogin QS var, show the login screen, otherwise
    show ICErepo
    Pass the username and password values through in the QS to the contents
    iFrame

--- index.php
+++ index.php
@@ -10,39 +10,61 @@
 <link rel="stylesheet" type="text/css" href="ice-repo.css">
 </head>
 
-<body style="margin: 0; overflow: hidden" onLoad="doRepo(get('repos').value)">
-	
-<div id="loadingMask" class="blackMask" style="visibility: visible" onContextMenu="return false">
-	<div class="popupVCenter">
-		<div class="popup">
-			<div class="circleOutside"></div>
-			<div class="circleInside"></div>
-			&nbsp;&nbsp;&nbsp;working...
-		</div>
-	</div>
-</div>
+<?php
+if (isset($_GET['sessionLogin'])) {
+?>
 
-<div class="header">
-	<select name="repos" id="repos" onChange="doRepo(this.value)" style="margin: 20px 0 0 20px">
-	<?php
-	for ($i=0;$i<count($repos);$i+=3) {
-		echo '<option id="repo'.($i/3).'" value="'.$repos[$i].'@'.$repos[$i+1].'"';
-		echo $repos[$i+2]=="selected" ? ' selected' : '';
-		echo '>'.$repos[$i]."</option>\n";
-	}
-	?>
-	</select>
-	
-	<div class="pullGithubSel" onClick="pullContent('selected')">Pull selected from Github</div>
-	<div class="version"><?php echo $version;?></div>
-	<img src="images/ice-repo.gif" alt="ICErepo" class="logo">
-</div>
-
-<form name="showRepo" action="contents.php" target="repo" method="POST">
-	<input type="hidden" name="repo" value="">
+<form name="sessionLogin" action="<?php echo $_SERVER['SCRIPT_NAME']; ?>" method="POST">
+	<b>Token:</b><br>
+	<input type="text" name="token">
+	<br><br>
+	or:<br><br>
+	<b>Username:</b><br>
+	<input type="text" name="username"><br>
+	<b>Password:</b><br>
+	<input type="text" name="password"><br>
+	<input type="submit">
 </form>
 
-<iframe id="repo" class="repoFrame" frameborder="0"></iframe>
+<?php
+} else {
+?>
+	<body style="margin: 0; overflow: hidden" onLoad="doRepo(get('repos').value)">
+	
+	<div id="loadingMask" class="blackMask" style="visibility: visible" onContextMenu="return false">
+		<div class="popupVCenter">
+			<div class="popup">
+				<div class="circleOutside"></div>
+				<div class="circleInside"></div>
+				&nbsp;&nbsp;&nbsp;working...
+			</div>
+		</div>
+	</div>
+
+	<div class="header">
+		<select name="repos" id="repos" onChange="doRepo(this.value)" style="margin: 20px 0 0 20px">
+		<?php
+		for ($i=0;$i<count($repos);$i+=3) {
+			echo '<option id="repo'.($i/3).'" value="'.$repos[$i].'@'.$repos[$i+1].'"';
+			echo $repos[$i+2]=="selected" ? ' selected' : '';
+			echo '>'.$repos[$i]."</option>\n";
+		}
+		?>
+		</select>
+	
+		<div class="pullGithubSel" onClick="pullContent('selected')">Pull selected from Github</div>
+		<div class="version"><?php echo $version;?></div>
+		<img src="images/ice-repo.gif" alt="ICErepo" class="logo">
+	</div>
+
+	<form name="showRepo" action="contents.php?username=<?php echo $username;?>&password=<?php echo $password;?>" target="repo" method="POST">
+		<input type="hidden" name="repo" value="">
+	</form>
+
+	<iframe id="repo" class="repoFrame" frameborder="0"></iframe>
+<?php
+;};
+?>
 	
 </body>
 
